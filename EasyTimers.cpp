63ae3d9d8ea3f2ec458360b695b8ee2dc687a3b9
DECE <tim.korolik@gmail.com>
2020-03-04 22:59:57 +0300

    Added EThread class with thread control

--- EasyTimers.cpp
+++ EasyTimers.cpp
@@ -1,23 +1,107 @@
 #include "EasyTimers.h"
 
-ETimer::ETimer(uint16_t delay)
-{
-    _triggerTime = 0;
-    _delay = delay;
+
+////////////////////////////////////////
+//               ETimer               //
+
+ETimer::ETimer() {
+  _last_trigger_time = 0;
+  _delay = 0;
 }
 
-void ETimer::setDelay(uint16_t delay)
-{
-    _delay = delay;
+ETimer::ETimer(uint16_t delay) {
+  _last_trigger_time = 0;
+  _delay = delay;
 }
 
-bool ETimer::check()
-{
-    if (millis() - _triggerTime >= _delay)
-    {
-        _triggerTime = millis();
-        return true;
-    }
-    else
-        return false;
+uint16_t ETimer::getDelay() {
+  return _delay;
 }
+
+uint32_t ETimer::getLastTriggerTime() {
+  return _last_trigger_time;
+}
+
+void ETimer::reset() {
+  _last_trigger_time = 0;
+}
+
+void ETimer::setDelay(uint16_t delay) {
+  _delay = delay;
+  reset();
+}
+
+bool ETimer::check() {
+  if (millis() - _last_trigger_time >= _delay) {
+    _last_trigger_time = millis();
+    return true;
+  } else
+    return false;
+}
+
+//               ETimer               //
+////////////////////////////////////////
+
+
+////////////////////////////////////////
+//               EThread              //
+
+EThread::EThread(uint16_t delay) {
+  timer.setDelay(delay);
+  state = STOPED;
+  _has_callback = false;
+}
+
+EThread::EThread(uint16_t delay, void (*func)(bool)) {
+  timer.setDelay(delay);
+  state = STOPED;
+  _callback_function = func;
+  _has_callback = true;
+}
+
+EThread::EThread(void (*func)(bool)) {
+  timer.setDelay(0);
+  state = STOPED;
+  _callback_function = func;
+  _has_callback = true;
+}
+
+bool EThread::hasCallback() {
+  return _callback_function;
+}
+
+bool EThread::getState() {
+  return state;
+}
+
+void EThread::setCallback(void (*func)(bool)) {
+  _callback_function = func;
+  _has_callback = true;
+}
+
+void EThread::start() {
+  state = RUNNING;
+  _execute_callback();
+}
+
+void EThread::pause() {
+  state = STOPED;
+  _execute_callback();
+}
+
+void EThread::update() {
+  if (state == true && timer.check() == true) {
+    _execute_callback();
+  }
+}
+
+void EThread::_execute_callback() {
+  if (_has_callback == true) {
+    _callback_function(state);
+  } else {
+    state = STOPED;
+  }
+}
+
+//               EThread              //
+////////////////////////////////////////
--- EasyTimers.h
+++ EasyTimers.h
@@ -2,16 +2,51 @@
 #define ETimers
 #include <Arduino.h>
 
+#define STOPED 0
+#define RUNNING 1
+
 class ETimer
 {
-    private:
-        uint32_t _triggerTime;
-        uint16_t _delay;
+  private:
+    uint32_t _last_trigger_time;
+    uint16_t _delay;
 
-    public:
-        ETimer(uint16_t);             // create timer with delay in millis
-        void setDelay(uint16_t);      // set timer delay in millis
-        bool check();                 // check timer ready
+  public:
+    ETimer();                      // create timer with 0 delay
+    ETimer(uint16_t);              // create timer with delay in millis
+
+    void setDelay(uint16_t);       // set timer delay in millis
+    uint16_t getDelay();           // get timer delay in millis
+    uint32_t getLastTriggerTime(); // get last trigger time
+
+    void reset();                  // reset last trigger time
+    bool check();                  // check timer ready
+};
+
+class EThread
+{
+  private:
+    void (*_callback_function)(bool state);
+    bool _has_callback;
+
+    void _execute_callback();
+
+  public:
+    ETimer timer;                  // timer object
+    bool   state;                  // state of thread (may be STOPED or RUNNING)
+
+    EThread(uint16_t);                            // create thread with delay in millis
+    EThread(uint16_t, void (*func)(bool state));  // create thread with delay in millis and callback function
+    EThread(void (*func)(bool state));            // create thread with callback function and 0 delay
+
+    void setCallback(void (*func)(bool state));   // set callback function
+
+    void start();                  // resume thread
+    void pause();                  // pause thread
+    void update();                 // process thread
+
+    bool hasCallback();            // returns true if callback function is set
+    bool getState();               // returns thread state
 };
 
 #endif
